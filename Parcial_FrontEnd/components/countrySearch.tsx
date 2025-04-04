23a7edd132c3c6d7ddbfc8dcdcf93c421a5405f7
GuillermoHerranzV <gherranzv@alumnos.nebrija.es>
2025-04-04 12:23:25 +0200

    Commit

--- Parcial_FrontEnd/components/countrySearch.tsx
+++ Parcial_FrontEnd/components/countrySearch.tsx
@@ -3,7 +3,7 @@
 export const CountrySearch = async (number: string) => {
 
     const apiKey = Deno.env.get ("API_key");
-    const url= 'https://api.api-ninjas.com/v1/validatephone?number=' + number,
+    const url= 'https://api.api-ninjas.com/v1/validatephone?number=' + number
 
     //const results = await fetch ({url, headers: {apiKey: '50LKoykoNG+hBf6kwV6Ugw==O30EjkaBL8l69lZK'}});
 
